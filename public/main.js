c5693d725551f469721b4c5a20410f405a01f4e0
Ruth Wu <rwu02@email.unc.edu>
2022-04-24 18:16:51 -0400

    one flip mostly working

--- public/main.js
+++ public/main.js
@@ -1,10 +1,5 @@
 // Focus div based on nav button click
 document.getElementById("homenav").onclick = function(){
-    document. location. reload();
-    document.getElementById("homenav").className = "active";
-    document.getElementById("singlenav").className = "";
-    document.getElementById("multinav").className = "";
-    document.getElementByIdii("guessnav").className = "";
     document.getElementById("home").className = "";
     document.getElementById("single").className = "hidden";
     document.getElementById("multi").className = "hidden";
@@ -12,10 +7,6 @@ document.getElementById("homenav").onclick = function(){
 };
 
 document.getElementById("singlenav").onclick = function(){
-    document.getElementById("homenav").className = "";
-    document.getElementById("singlenav").className = "active";
-    document.getElementById("multinav").className = "";
-    document.getElementById("guessnav").className = "";
     document.getElementById("home").className = "hidden";
     document.getElementById("single").className = "";
     document.getElementById("multi").className = "hidden";
@@ -23,10 +14,6 @@ document.getElementById("singlenav").onclick = function(){
 };
 
 document.getElementById("multinav").onclick = function(){
-    document.getElementById("homenav").className = "";
-    document.getElementById("singlenav").className = "";
-    document.getElementById("multinav").className = "active";
-    document.getElementById("guessnav").className = "";
     document.getElementById("home").className = "hidden";
     document.getElementById("single").className = "hidden";
     document.getElementById("multi").className = "";
@@ -34,10 +21,6 @@ document.getElementById("multinav").onclick = function(){
 };
 
 document.getElementById("guessnav").onclick = function(){
-    document.getElementById("homenav").className = "";
-    document.getElementById("singlenav").className = "";
-    document.getElementById("multinav").className = "";
-    document.getElementById("guessnav").className = "active";
     document.getElementById("home").className = "hidden";
     document.getElementById("single").className = "hidden";
     document.getElementById("multi").className = "hidden";
@@ -45,73 +28,19 @@ document.getElementById("guessnav").onclick = function(){
 };
 
 // Flip one coin and show coin image to match result when button clicked
-// Event listener for whatever is being clicked 
-//			document.addEventListener("click", activeNow);
-// Replace text in anything with "active" id
-			// function activeNow() {
-			// 	const active_now = document.activeElement
-			// 	document.getElementById("active").innerHTML = active_now;
-			// 	console.log(active_now)
-			// }
-// Button coin flip element
-const coin = document.getElementById("coin")
-// Add event listener for coin button
-coin.addEventListener("click", flipCoin)
 function flipCoin() {
-    fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
+    fetch('http://localhost:5555/app/flip/', {mode: 'cors'})
         .then(function(response) {
             return response.json();
         })
         .then(function(result) {
             console.log(result);
             document.getElementById("result").innerHTML = result.flip;
-            document.getElementById("quarter").setAttribute("src", result.flip+".jpg");
+            document.getElementById("quarter").setAttribute("src", "./assets/img/"+result.flip+".jpg");
             coin.disabled = true
         })
 }
-
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
-const coins = document.getElementById("coins")
-// Add event listener for coins form
-coins.addEventListener("submit", flipCoins)
-// Create the submit handler
-async function flipCoins(event) {
-    event.preventDefault();
-    
-    const endpoint = "app/flip/coins/"
-    const url = document.baseURI+endpoint
-
-    const formEvent = event.currentTarget
-
-    try {
-        const formData = new FormData(formEvent);
-        const flips = await sendFlips({ url, formData });
-
-        console.log(flips);
-        document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
-        document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
-    } catch (error) {
-        console.log(error);
-    }
-}
-// Create a data sender
-async function sendFlips({ url, formData }) {
-    const plainFormData = Object.fromEntries(formData.entries());
-    const formDataJson = JSON.stringify(plainFormData);
-    console.log(formDataJson);
-
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        },
-        body: formDataJson
-    };
-
-    const response = await fetch(url, options);
-    return response.json()
-}
 
 // Guess a flip by clicking either heads or tails button
