902e0ba6a3d96618b32ee108e230bf88b372ecb8
Ruth Wu <rwu02@email.unc.edu>
2022-04-24 21:41:14 -0400

    flip many working

--- public/main.js
+++ public/main.js
@@ -30,23 +30,60 @@ document.getElementById("guessnav").onclick = function(){
 // Flip one coin and show coin image to match result when button clicked
 function flipCoin() {
     fetch('http://localhost:5555/app/flip/')
-    .then((response) => {
-        return response.json();
-    })
-    .then((result) => {
-        console.log(result);
-        document.getElementById("result").innerHTML = result.flip;
-        if(result.flip === "heads"){
-            document.getElementById("heads").style.display = 'block';
-            document.getElementById("tails").style.display = 'none';
-        } else if(result.flip === "tails"){
-            document.getElementById("heads").style.display = 'none';
-            document.getElementById("tails").style.display = 'block';
-        }
-        document.getElementById("coin").style.display = 'none';
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+            document.getElementById("result").innerHTML = result.flip;
+            if(result.flip === "heads"){
+                document.getElementById("heads").style.display = 'block';
+                document.getElementById("tails").style.display = 'none';
+            } else if(result.flip === "tails"){
+                document.getElementById("heads").style.display = 'none';
+                document.getElementById("tails").style.display = 'block';
+            }
+            document.getElementById("coin").style.display = 'none';
+        })
 }
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+async function flipManyCoins() {
+    var num = document.getElementById("number").value;
+    fetch('http://localhost:5555/app/flip/coins', {
+        body: JSON.stringify({"number": num}),
+        headers: {"Content-Type": "application/json",},
+        method: "post"
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+            document.getElementById("results_heads").innerHTML = result.summary.heads;
+            document.getElementById("results_tails").innerHTML = result.summary.tails;
+            
+            var chart = document.getElementById("chart");
+            chart.innerHTML = '';
+            var data = document.createElement("table");
+            var row = data.insertRow();
+            const arr = result.raw;
+            var i = 0;
+            while(i < arr.length){
+                var row = data.insertRow();
+                for(var j = 0; j < 5; j++){
+                    if(i < arr.length){
+                        var cell = row.insertCell();
+                        var image = document.createElement('img');
+                        image.setAttribute("src", "/assets/img/"+arr[i]+".png");
+                        image.setAttribute("class", "smallcoin");
+                        cell.appendChild(image); 
+                        i++;
+                    }
+                }
+            }
+            chart.appendChild(data);
+        })
+}
 
 // Guess a flip by clicking either heads or tails button
